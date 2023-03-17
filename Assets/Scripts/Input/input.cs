0fed5fc5b4824a776ac4b26dbff11ff25dd4be28
Xi Liu <xi1331@126.com>
2023-03-17 11:58:57 -0400

    update

---
+++ Assets/Scripts/Input/input.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class input : MonoBehaviour
+{
+    public float speed = 1f,
+    sensitivity = 10f;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+        }
+        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+        }
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
+        }
+        if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
+        }
+        if(Input.GetKey(KeyCode.Alpha1) || Input.GetKey(KeyCode.Keypad1))
+        {
+            transform.Translate(new Vector3(0,0,speed * Time.deltaTime));
+        }
+        if(Input.GetKey(KeyCode.Alpha2) || Input.GetKey(KeyCode.Keypad2))
+        {
+            transform.Translate(new Vector3(0,0,-speed * Time.deltaTime));
+        }
+
+        Transform c = Camera.main.transform;
+        c.Rotate(0, Input.GetAxis("Mouse X") * sensitivity, 0);
+        c.Rotate(-Input.GetAxis("Mouse Y") * sensitivity, 0, 0);
+        c.Rotate(0, 0, -Input.GetAxis("QandE") * 90 * Time.deltaTime);
+        if(Input.GetMouseButtonDown(0))
+            Cursor.lockState = CursorLockMode.Locked;
+    }
+}
