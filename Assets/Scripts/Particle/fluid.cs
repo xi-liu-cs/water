cda024efd1634a9e595beab2cfde02e9a2514fbe
Xi Liu <xi1331@126.com>
2023-03-29 23:03:29 -0400

    update

--- Assets/Scripts/Particle/fluid.cs
+++ Assets/Scripts/Particle/fluid.cs
@@ -10,39 +10,40 @@ public class fluid : MonoBehaviour
     public Material material;
     public float mass = 4f,
     viscosity = 2.5f,
-    particle_size = 1f,
+    particle_size = 2f,
     radius = 1f,
     gas_constant = 2000f,
     dt = 0.0008f,
     rest_density = 1f,
     damping = -0.5f;
-    public Vector3 g = new Vector3(0f, -9.81f, 0f);
+    public Vector3 offset,
+    g = new Vector3(0f, -9.81f, 0f);
 
     [Header("fluid")]
-    public int n_particle = 1000,
-    dimension = 10,
+    public int n_particle = 50000,
+    dimension = 100,
     maximum_particle_per_cell = 500;
 
     [StructLayout(LayoutKind.Sequential, Size = 28)]
-    struct particle
+    public struct particle
     {
-        Vector3 position;
-        Vector4 color;
+        public Vector3 position;
+        public Vector4 color;
     }
+
+    particle[] particles;
+
+    int size_property = Shader.PropertyToID("size"),
+    particle_buffer_property = Shader.PropertyToID("particle_buffer");
 
     public ComputeShader compute_shader;
 
-    ComputeBuffer particles,
-    arg_buf,
-    density,
-    pressure,
-    force,
-    velocity;
-
-    int malloc_particle_kernel;
+    ComputeBuffer particle_buffer,
+    arg_buffer;
 
     void Awake()
     {
+        malloc_particle();
         find_kernel();
         compute_shader_init();
         compute_buffer_init();
@@ -50,19 +51,42 @@ public class fluid : MonoBehaviour
 
     void Update()
     {
-        compute_shader.Dispatch(malloc_particle_kernel, 1, 1, 1);
-        Graphics.DrawMeshInstancedIndirect(particle_mesh, 0, material, new Bounds(Vector3.zero, new Vector3(100f, 100f, 100f)), arg_buf, castShadows: UnityEngine.Rendering.ShadowCastingMode.Off);
+        material.SetFloat(size_property, particle_size);
+        material.SetBuffer(particle_buffer_property, particle_buffer);
+        Graphics.DrawMeshInstancedIndirect(particle_mesh, 0, material, new Bounds(Vector3.zero, new Vector3(100f, 100f, 100f)), arg_buffer, castShadows: UnityEngine.Rendering.ShadowCastingMode.Off);
+    }
+
+    void malloc_particle()
+    {
+        particles = new particle[n_particle];
+        int particle_per_dimension = Mathf.CeilToInt(Mathf.Pow(n_particle, 1f / 3f)),
+        i = 0;
+        while(i < n_particle)
+        {
+            for(int x = 0; x < particle_per_dimension; ++x)
+                for(int y = 0; y < particle_per_dimension; ++y)
+                    for(int z = 0; z < particle_per_dimension; ++z)
+                    {
+                        float r = Random.Range(0f, 5f);
+                        Vector3 pos = new Vector3(1.5f * r * x, r * y, r * z) + offset;
+                        particles[i] = new particle
+                        {
+                            position = pos,
+                            color = new Vector4(0.3f, 0.5f, 1f, 0.5f)
+                        };
+                        if(++i == n_particle) return;
+                    }
+        }
     }
 
     void find_kernel()
     {
-        malloc_particle_kernel = compute_shader.FindKernel("malloc_particle");
+
     }
 
     void compute_shader_init()
     {
-        compute_shader.SetInt("n_particle", n_particle);
-        compute_shader.SetInt("dimension", dimension);
+
     }
 
     unsafe void compute_buffer_init()
@@ -75,10 +99,10 @@ public class fluid : MonoBehaviour
             particle_mesh.GetBaseVertex(0),
             0
         };
-        particles = new ComputeBuffer(n_particle, sizeof(particle));
-        arg_buf = new ComputeBuffer(1, arg.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
-        arg_buf.SetData(arg);
-        compute_shader.SetBuffer(malloc_particle_kernel, "particles", particles);
+        particle_buffer = new ComputeBuffer(n_particle, sizeof(particle));
+        particle_buffer.SetData(particles);
+        arg_buffer = new ComputeBuffer(1, arg.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        arg_buffer.SetData(arg);
     }
 
     void OnDestroy()
@@ -88,6 +112,6 @@ public class fluid : MonoBehaviour
 
     void free()
     {
-        particles.Dispose();
+        particle_buffer.Dispose();
     }
 }
