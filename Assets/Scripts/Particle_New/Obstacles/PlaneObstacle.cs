d9365c4d956ed5f2d84a591ab1c790bbab12cc38
Ryan Kim <kimryan0416@gmail.com>
2023-05-17 01:34:44 -0400

    testing new system for plane detection and particle interaction

--- Assets/Scripts/Particle_New/Obstacles/PlaneObstacle.cs
+++ Assets/Scripts/Particle_New/Obstacles/PlaneObstacle.cs
@@ -8,25 +8,49 @@ public class PlaneObstacle : MonoBehaviour
 
     public Vector3 size;
     public float radius;
-    public Transform target;
+    [ReadOnly] public Vector3 forward, up, right;
+    public Transform normalTarget, particleTarget;
+    [ReadOnly] public Vector3 normalVector, targetVector, projectionPoint;
+    [ReadOnly] public float dotBetweenParticleAndNormal;
 
     public bool isIntersecting = false;
 
+    void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + normalVector);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + targetVector);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(projectionPoint, 0.1f);
+    }
+
     // Update is called once per frame
     void Update() {
-        size = 10f * new Vector3(
-            transform.lossyScale.z, 
+        // https://forum.unity.com/threads/projection-of-point-on-plane.855958/
+        targetVector = particleTarget.position - transform.position;
+        normalVector = normalTarget.position - transform.position;
+        dotBetweenParticleAndNormal = Vector3.Dot(targetVector, normalVector);
+        projectionPoint = ClosestPointOnPlane(targetVector, normalVector, particleTarget.position);
+        /*
+        size = new Vector3(
             transform.lossyScale.x, 
-            transform.lossyScale.y / 5
+            transform.lossyScale.y, 
+            transform.lossyScale.z
         );
         if (target.hasChanged || transform.hasChanged) {
-            Debug.Log("CHANGING");
+            
+            up = transform.up;
+            forward = -1f * transform.forward;
+            right = transform.right;
+
             Vector3 targetPosition = target.position - transform.position;
-            float d = Vector3.Dot(targetPosition, transform.up);
+            float d = Vector3.Dot(targetPosition, forward);
 
             Vector2 projection = new Vector2(
-                Vector3.Dot(targetPosition, transform.forward),
-                Vector3.Dot(targetPosition, transform.right)
+                Vector3.Dot(targetPosition, up),
+                Vector3.Dot(targetPosition, right)
             );
 
             Vector2 start = new Vector2(size.x, size.y)/(-2f) - new Vector2(radius, radius);
@@ -37,5 +61,14 @@ public class PlaneObstacle : MonoBehaviour
             target.hasChanged = false;
             transform.hasChanged = false;
         }
+        */
+    }
+
+    public Vector3 ClosestPointOnPlane(Vector3 planeOffset, Vector3 planeNormal, Vector3 point) {
+        return point + DistanceFromPlane(planeOffset, planeNormal, point) * planeNormal;
+    }
+
+    public float DistanceFromPlane(Vector3 planeOffset, Vector3 planeNormal, Vector3 point) {
+        return Vector3.Dot(planeOffset - point, planeNormal);
     }
 }
