aea25fc03be91ed4e243e938c95be85ba51732c0
Ryan Kim <kimryan0416@gmail.com>
2023-05-17 19:42:50 -0400

    added ability for objects with meshes to detect particles inside or intersecting with them

--- Assets/Scripts/Particle_New/Obstacles/PlaneObstacle.cs
+++ Assets/Scripts/Particle_New/Obstacles/PlaneObstacle.cs
@@ -2,84 +2,112 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
 [ExecuteInEditMode]
-public class PlaneObstacleTest : MonoBehaviour
+public class PlaneObstacle : MonoBehaviour
 {
-    public Transform[] vertices = new Transform[3];
-    public Vector3 normalVector;
-    public Vector3 size;
-    public float radius;
-    public Transform particleTarget;
-    [ReadOnly] public Vector3 centroid, targetVector, projectionPoint;
-    [ReadOnly] public float dotBetweenParticleAndNormal;
 
-    public bool isIntersecting = false;
-
-    void OnDrawGizmos() {
-        Gizmos.color = Color.white;
-        Gizmos.DrawSphere(centroid, 0.05f);
-
-        Gizmos.color = Color.green;
-        Gizmos.DrawSphere(vertices[0].position, 0.05f);
-        Gizmos.DrawSphere(vertices[1].position, 0.05f);
-        Gizmos.DrawSphere(vertices[2].position, 0.05f);
-
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(centroid, centroid + normalVector);
-
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(centroid, centroid + targetVector);
-
-        Gizmos.color = (isIntersecting) ? Color.red : Color.black;
-        Gizmos.DrawSphere(projectionPoint, 0.1f);
+    public struct ObsPlane {
+        public Vector3[] vertices;
+        public Vector3 centroid;
+        public Vector3 normalVector;
     }
 
-    // Update is called once per frame
-    void Update() {
-        centroid = (vertices[0].position + vertices[1].position + vertices[2].position)/3f;
-        normalVector = (vertices[0].forward + vertices[1].forward + vertices[2].forward)/3f;
-        normalVector = normalVector.normalized;
+    public enum ObstacleType {
+        Static,
+        OnTransformChange
+    }
 
-        // https://forum.unity.com/threads/projection-of-point-on-plane.855958/
-        targetVector = (particleTarget.position - centroid).normalized;
-        dotBetweenParticleAndNormal = Vector3.Dot(targetVector, normalVector);
-        projectionPoint = ClosestPointOnPlane(centroid, normalVector, particleTarget.position);
+    [ReadOnly, SerializeField] private MeshFilter meshFilter;
+    [ReadOnly, SerializeField] private  MeshRenderer meshRenderer;
+    public ObstacleType obstacleType = ObstacleType.Static;
+    public ObsPlane[] obstaclePlanes;
+
+    public Transform particle = null;
+    private bool isIntersecting = false;
+
+    void OnDrawGizmos() {
+        if (obstaclePlanes.Length == 0) return;
+        foreach(ObsPlane p in obstaclePlanes) {
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(transform.TransformPoint(p.vertices[0]),0.01f);
+            Gizmos.DrawSphere(transform.TransformPoint(p.vertices[1]),0.01f);
+            Gizmos.DrawSphere(transform.TransformPoint(p.vertices[2]),0.01f);
+            Gizmos.color = (isIntersecting) ? Color.yellow : Color.red;
+            Gizmos.DrawSphere(transform.TransformPoint(p.centroid),0.01f);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.TransformPoint(p.centroid), transform.TransformPoint(p.centroid + p.normalVector));
+        }
+    }
+
+    
+    private void Awake() {
+        meshFilter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        GeneratePlanes();
+    }
+
+    private void Update() {
+        if (obstacleType != ObstacleType.Static && transform.hasChanged) {
+            GeneratePlanes();
+            transform.hasChanged = false;
+        }
+
+        if (particle == null) return;
+
+        // First, find closest centroid to current target
+        ObsPlane closest = obstaclePlanes[FindClosestPlane(particle.position)];
+
+        // Secondly, Calculate dot product between normal vector and vector b/w centroid and target
+        Vector3 targetVector = (particle.position - transform.TransformPoint(closest.centroid)).normalized;
+        float dotBetweenParticleAndNormal = Vector3.Dot(targetVector, transform.TransformVector(closest.normalVector));
+        
+        // Thirdly, calculate projection onto plane
+        Vector3 projectionPoint = ClosestPointOnPlane(transform.TransformPoint(closest.centroid), transform.TransformVector(closest.normalVector), particle.position);
+        
+        // Finally, check if intersecting or not
         isIntersecting = dotBetweenParticleAndNormal <= 0f 
             && ObstacleHelper.PointInTriangle(
                 projectionPoint, 
-                vertices[0].position, 
-                vertices[1].position, 
-                vertices[2].position
-            );
-        /*
-        size = new Vector3(
-            transform.lossyScale.x, 
-            transform.lossyScale.y, 
-            transform.lossyScale.z
+                transform.TransformPoint(closest.vertices[0]), 
+                transform.TransformPoint(closest.vertices[1]), 
+                transform.TransformPoint(closest.vertices[2])
         );
-        if (target.hasChanged || transform.hasChanged) {
-            
-            up = transform.up;
-            forward = -1f * transform.forward;
-            right = transform.right;
+    }
 
-            Vector3 targetPosition = target.position - transform.position;
-            float d = Vector3.Dot(targetPosition, forward);
-
-            Vector2 projection = new Vector2(
-                Vector3.Dot(targetPosition, up),
-                Vector3.Dot(targetPosition, right)
-            );
-
-            Vector2 start = new Vector2(size.x, size.y)/(-2f) - new Vector2(radius, radius);
-            Vector2 end = new Vector2(size.x, size.y)/(2f) + new Vector2(radius, radius);
-
-            isIntersecting = (d <= 0 && d >= -size.z && projection.x >= start.x && projection.y >= start.y && projection.x <= end.x && projection.y <= end.y);
-
-            target.hasChanged = false;
-            transform.hasChanged = false;
+    private void GeneratePlanes() {
+        Mesh mesh = meshFilter.sharedMesh;
+        Vector3 centroid, normalVector;
+        ObsPlane newPlane;
+        
+        obstaclePlanes = new ObsPlane[mesh.triangles.Length / 3];
+        for(int t = 0; t < mesh.triangles.Length; t+=3) {
+            centroid = (mesh.vertices[mesh.triangles[t]] + mesh.vertices[mesh.triangles[t+1]] + mesh.vertices[mesh.triangles[t+2]])/3f;
+            normalVector = (mesh.normals[mesh.triangles[t]] + mesh.normals[mesh.triangles[t+1]] + mesh.normals[mesh.triangles[t+2]])/3f;
+            newPlane = new ObsPlane();
+            newPlane.vertices = new Vector3[3]{
+                mesh.vertices[mesh.triangles[t]],
+                mesh.vertices[mesh.triangles[t+1]],
+                mesh.vertices[mesh.triangles[t+2]]
+            };
+            newPlane.centroid = centroid;
+            newPlane.normalVector = normalVector;
+            obstaclePlanes[t/3] = newPlane;
         }
-        */
+    }
+
+    private int FindClosestPlane(Vector3 t) {
+        int closest = 0;
+        float distance, closestDistance = Mathf.Infinity;
+        for(int i = 0; i < obstaclePlanes.Length; i++) {
+            distance = Vector3.Distance(t, transform.TransformPoint(obstaclePlanes[i].centroid));
+            if (distance < closestDistance) {
+                closestDistance = distance;
+                closest = i;
+            }
+        }
+        return closest;
     }
 
     public Vector3 ClosestPointOnPlane(Vector3 planeOffset, Vector3 planeNormal, Vector3 point) {
